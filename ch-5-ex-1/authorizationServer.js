49ce6fedca70b775a20917204f42ad043d34a8bf
WATANAPEI <info@wpei.dev>
2020-12-27 03:20:31 +0000

    5-2

--- ch-5-ex-1/authorizationServer.js
+++ ch-5-ex-1/authorizationServer.js
@@ -63,10 +63,34 @@ app.get("/authorize", function(req, res){
 });
 
 app.post('/approve', function(req, res) {
+	var reqid = req.body.reqid;
+	var query = requests[reqid];
+	delete requests[reqid];
 
-	/*
-	 * Process the results of the approval page, authorize the client
-	 */
+	if(!query) {
+		res.render('error', { error: 'No matching authorization request' });
+		return;
+	}
+	if(req.body.approve) {
+		if(query.response_type == 'code') {
+			var code = randomstring.generate(8);
+			codes[code] = { request: query};
+			var urlParsed = buildUrl(query.redirect_uri, {code: code, state: query.state});
+			res.redirect(urlParsed);
+			return;
+
+		} else {
+			var urlParsed = buildUrl(query.redirect_uri, {error: 'unsupported_response_type'});
+			res.redirect(urlParsed);
+			return;
+		}
+
+	} else {
+		var urlParsed = buildUrl(query.redirect_uri, { error: 'access_denied'});
+		res.redirect(urlParsed);
+		return;
+	}
+
 
 });
 
